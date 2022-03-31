f79cb8ad14a8ba66d865327a9f8e3f0659ea532e
BriSalcido07 <91872636+BSalcido@users.noreply.github.com>
2022-03-31 16:23:17 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="App-header text-center">Dictionary</h1>
+    </div>
+  );
+}
+
+export default App;
