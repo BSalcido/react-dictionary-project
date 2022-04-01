05743d44e555db67dcd09182a9005c0c3ceefd74
BriSalcido07 <91872636+BSalcido@users.noreply.github.com>
2022-04-01 11:11:47 +0200

    Added form functionality

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Dictionary from "./Dictionary";
 function App() {
   return (
     <div className="App">
-      <h1 className="App-header text-center">Dictionary</h1>
-      <Dictionary />
-      <hr className="mb-0 mt-5" />
-      <footer>Coded by Brisa</footer>
+      <div className="container">
+        <h1 className="App-header text-center">Dictionary</h1>
+        <Dictionary />
+        <hr className="mb-0 mt-5" />
+        <footer>Coded by Brisa</footer>
+      </div>
     </div>
   );
 }
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,22 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Dictionary.css";
 
 function Dictionary() {
+  const [word, setWord] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching...${word}`);
+  }
+
+  function handleWordInput(event) {
+    setWord(event.target.value);
+  }
+
   return (
     <div className="Dictionary">
       <div className="Dictionary__search-engine">
-        <form className="Dictionary__search-form row">
+        <form className="Dictionary__search-form row" onSubmit={search}>
           <div className="col-md-9 col-lg-10 col-xl-11">
             <input
               type="text"
               className="form-control"
               placeholder="What word do you want to look up?"
+              onChange={handleWordInput}
             ></input>
           </div>
           <div className="col-md-3 col-lg-2 col-xl-1 d-flex justify-content-center">
-            <button className="btn Dictionary__btn" type="submit">
-              Search
-            </button>
+            <input
+              className="Dictionary__btn btn"
+              type="submit"
+              value="Search"
+            />
           </div>
         </form>
       </div>
