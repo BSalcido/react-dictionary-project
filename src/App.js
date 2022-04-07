b09dfbb43e33bbd23702e4ff73a7d072609ba2f3
BriSalcido07 <91872636+BSalcido@users.noreply.github.com>
2022-04-07 16:35:34 +0200

    Updated footer

--- src/App.js
+++ src/App.js
@@ -6,7 +6,25 @@ function App() {
     <div className="App">
       <div className="container">
         <Dictionary />
-        <footer>Coded by Brisa</footer>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/brisa-salcido-0ba125186"
+            target={"_blank"}
+            rel="noreferrer"
+          >
+            Brisa Salcido
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/BSalcido/react-dictionary-project"
+            target={"_blank"}
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and hosted on Netlify
+        </footer>
       </div>
     </div>
   );
