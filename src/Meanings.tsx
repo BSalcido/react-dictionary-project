619f022957e04efa49a53b2e763ba3384d66b253
BriSalcido07 <91872636+BSalcido@users.noreply.github.com>
2022-04-26 17:51:58 +0200

    Migrate Meanings to TSX

---
+++ src/Meanings.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Meanings.css";
+import Example from "./Example";
+import Synonyms from "./Synonyms";
+import Antonyms from "./Antonyms";
+
+type Definition = { definition: string; example: string };
+
+type Props = {
+  meanings: {
+    partOfSpeech: string;
+    definitions: Definition[];
+    synonyms: string[];
+    antonyms: string[];
+  };
+};
+
+const Meanings = (props: Props) => (
+  <div className="Meanings">
+    <h3 className="Meanings__part">{props.meanings.partOfSpeech}</h3>
+    {props.meanings.definitions.map((definition, index) => {
+      return (
+        <div className="Meanings__definitions px-3" key={index}>
+          <p className="Meanings__definition">
+            <span className="me-3"> {`${index + 1}.`}</span>
+            {definition.definition}
+          </p>
+          <Example example={definition.example} />
+        </div>
+      );
+    })}
+
+    <Synonyms synonyms={props.meanings.synonyms} />
+    <Antonyms antonyms={props.meanings.antonyms} />
+  </div>
+);
+
+export default Meanings;
